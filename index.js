a8bf971b64a55066561f5a5c8a7a9c56f22882d4
Siddhartha <siddhartha@gozoomo.com>
2017-04-07 23:29:42 +0530

    Adding initial HF Calls

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 'use strict';
 
 // Instantiate all Hyperledger Fabric Client objects
+// Hardcoding these paths for now, until I figure out if thes
+// artifacts are downloadable from anywhere or if I should 
+// be pointing them to a Github URL.
 var hfc = require("/home/siddhartha/go/src/github.com/hyperledger/fabric-sdk-node/node_modules/fabric-client")
 var utils = require("/home/siddhartha/go/src/github.com/hyperledger/fabric-sdk-node/node_modules/fabric-client/lib/utils.js")
-var peer = require('/home/siddhartha/go/src/github.com/hyperledger/fabric-sdk-node/node_modules/fabric-client/lib/Peer.js');
-var orderer = require('/home/siddhartha/go/src/github.com/hyperledger/fabric-sdk-node/node_modules/fabric-client/lib/Orderer.js');
+var Peer = require('/home/siddhartha/go/src/github.com/hyperledger/fabric-sdk-node/node_modules/fabric-client/lib/Peer.js');
+var Orderer = require('/home/siddhartha/go/src/github.com/hyperledger/fabric-sdk-node/node_modules/fabric-client/lib/Orderer.js');
 var eventHub = require('/home/siddhartha/go/src/github.com/hyperledger/fabric-sdk-node/node_modules/fabric-client/lib/EventHub.js')
 
+// Hyperledger Fabric Configuration information
 var config = require('./config.json');
 
 var log4js = require('log4js');
@@ -23,5 +27,16 @@ app.get('/', function (req, res) {
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+	logger.debug('Example app listening on port 3000!')
+
+	// Construct the entire Fabric here
+	// This will be the Fabric Administration node
+  	var chain = client.newChain(config.chainName);
+	chain.addOrderer(new Orderer(config.orderer.orderer_url));
+	logger.debug('.. Adding new Orderer at: ' + config.orderer.orderer_url);
+
+	for (var i = 0; i < config.peers.length; i++) {
+		logger.debug('.. Adding new Peer at: ' + config.peers[i].peer_url);
+		chain.addPeer(new Peer(config.peers[i].peer_url));
+	}
 })
